ab8ad958621346cd9076aaf134992bb26421ad7e
Faizal Sulthan <faizalsulthan2@gmail.com>
2026-03-29 11:47:31 +0530

    worked on exceptions and changed the application properties to .yaml format

--- src/main/java/com/faizal/springboot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/faizal/springboot/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.faizal.springboot.exception;
 import com.faizal.springboot.dto.ErrorResponseDTO;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -18,14 +19,19 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ErrorResponseDTO handleValidationException(MethodArgumentNotValidException e){
+    @ExceptionHandler({MethodArgumentNotValidException.class, ResourceNotFoundException.class})
+    public ResponseEntity<ErrorResponseDTO> handleValidationException(MethodArgumentNotValidException e){
         BindingResult result = e.getBindingResult();
         Map<String, String> errors = new HashMap<>();
         for (FieldError error : result.getFieldErrors()){
             errors.put(error.getField(), error.getDefaultMessage());
         }
-        return new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(), "Invalid arguments", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponseDTO(HttpStatus.BAD_REQUEST.value(), "Invalid arguments", errors));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> GeneralException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponseDTO(HttpStatus
+                .INTERNAL_SERVER_ERROR.value(), ex.getMessage(),null));
     }
 }
